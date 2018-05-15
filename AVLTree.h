5ded3b2ece8c887834f4e64c8c5ede7b28b90ad1
DuParkLinux <dupark3@gmail.com>
2018-05-15 07:50:31 -0400

    fixAVL function checks if the insertion caused its grandparent to become non-AVL

--- AVLTree.h
+++ AVLTree.h
@@ -1,3 +1,4 @@
+#include <cmath> // abs
 #include <iostream>
 
 // AVL tree has no greater than a difference of 1 in subtree size to its left and right
@@ -19,6 +20,8 @@ private:
     void insert(Node<T>*, Node<T>*, T);
     void right_rotate(Node<T>*);
     void left_rotate(Node<T>*);
+    void fix_AVL(Node<T>*);
+    void rotate(Node<T>*, Node<T>*, Node<T>*);
     void print_sorted(Node<T>*);
 };
 
@@ -74,7 +77,27 @@ void AVLTree<T>::insert(Node<T>* node, Node<T>* parent, T val){
             parent->left = node;
         else if (val > parent->value)
             parent->right = node;
+        fix_AVL(node);
     }
+}
+
+template <class T>
+void AVLTree<T>::fix_AVL(Node<T>* node){
+    int left_height = -1;
+    int right_height = -1;
+    Node<T>* grandparent = node->parent->parent;
+    Node<T>* parent = node->parent;
+    if(parent && grandparent && grandparent->left)
+        left_height = grandparent->left->height;
+    if(parent && grandparent && grandparent->right)
+        right_height = grandparent->right->height;
+    if(std::abs(left_height - right_height) > 1)
+        rotate(grandparent, parent, node);
+}
+
+template <class T>
+void AVLTree<T>::rotate(Node<T>* grandparent, Node<T>* parent, Node<T>* child){
+
 }
 
 template <class T>
--- main.cpp
+++ main.cpp
@@ -7,7 +7,10 @@ int main(){
     AVLTree<int> my_AVL_tree(5);
     my_AVL_tree.insert(7);
     my_AVL_tree.insert(3);
+    my_AVL_tree.print_sorted(); // 3 5 7, AVL-true
+    my_AVL_tree.insert(9);
+    my_AVL_tree.insert(11); // 3 5 7 9 11, AVL-false
     my_AVL_tree.print_sorted();
-    
+
     return 0;
 }
